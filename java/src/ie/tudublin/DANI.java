f325873cbe8a548716686ec49df2b1a32a443dfc
Lauraawei <C21371683@mytudublin.ie>
2023-04-26 18:17:34 +0100

    loll

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -6,28 +6,69 @@ import processing.core.PApplet;
 
 public class DANI extends PApplet {
 
-	
+	String[] sonnet;
+	ArrayList<Word> model = new ArrayList<Word>();
 
+	
 	public void settings() {
 		size(1000, 1000);
 		//fullScreen(SPAN);
 	}
 
-    String[] sonnet;
-
-    public String[] writeSonnet()
-    {
-        return null;
-    }
 
 	public void setup() {
 		colorMode(HSB);
-         
-       
+		loadFile(); 
 	}
 
 	public void keyPressed() {
+		if (key == ' ') {
+			sonnet = writeSonnet();
+			for (String line : sonnet) 
+			{
+			  System.out.println(line);
+			}
 
+		  }
+	}
+
+	public String[] writeSonnet() {
+		String[] sonnet = new String[14];
+		for (int i = 0; i < 14; i++) {
+			StringBuilder sb = new StringBuilder();
+			Word word = model.get((int) random(model.size())); // pick a random starting word
+			sb.append(word.getWord()); // add the starting word to the sentence
+			int count = 1;
+			while (count < 8 && !word.getFollows().isEmpty()) { // repeat until 8 words or no follows
+				Follow follow = word.getFollows().get((int) random(word.getFollows().size())); // pick a random follow
+				sb.append(" ").append(follow.getWord()); // add the follow to the sentence
+				count++;
+				word = findWord(follow.getWord()); // set the current word to the follow
+			}
+			sonnet[i] = sb.toString();
+		}
+		return sonnet;
+	}
+
+	public Word findWord(String word) {
+		for (Word w : model) {
+		  if (w.getWord().equals(word)) {
+			return w;
+		  }
+		  
+		}
+		return null;
+	  }	
+
+	  public void loadFile() {
+		String[] lines = loadStrings("small.txt");
+		for (String line : lines) {
+			String[] words = split(line, ',');
+			for (String w : words) {
+				w = w.replaceAll("[^\\w\\s]","");
+				w = w.toLowerCase();
+			}
+		}
 	}
 
 	public class Follow {
@@ -84,18 +125,9 @@ public class DANI extends PApplet {
 			sb.append("]");
 			return sb.toString();
 		}
-
-
-		public void loadFile() {
-
-		loadStrings("filename.txt");
-		split(line, '');
-		w.replaceAll("[^\\w\\s]","");
-		s.toLowerCase()
-
-		}
-
-	float off = 0;
+		
+		
+	//float off = 0;
 
 	public void draw() 
     {
@@ -104,9 +136,27 @@ public class DANI extends PApplet {
 		noStroke();
 		textSize(20);
         textAlign(CENTER, CENTER);
+		String[] sonnet = writeSonnet();
+    for (int i = 0; i < sonnet.length; i++) {
+        text(sonnet[i], width/2, height/14*(i+1));
+    }
         
 	}
 
+	public void printModel() {
+		for (Word word : model) {
+			System.out.print(word.getWord() + ": ");
+			for (Follow follow : word.getFollows()) {
+				System.out.print(follow.getWord() + "(" + follow.getCount() + ") ");
+			}
+			System.out.println();
+		}
+	}
 
 }
+
+public static void main(String[] args) {
+	PApplet.main("ie.tudublin.DANI");
+}
+	
 }
